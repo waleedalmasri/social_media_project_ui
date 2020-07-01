941fb9477498bf3e3fd98016933453ec15b7c84b
waleedalmasri <67055177+waleedalmasri@users.noreply.github.com>
2020-07-01 11:30:08 +0300

    header-routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path={'/'} exact component={Home}></Route>
+          <Route path={'/profile'} component={Profile}></Route>
+          <Redirect to={'/'}></Redirect>
+        </Switch>
+      </Router>
     </div>
   );
 }
